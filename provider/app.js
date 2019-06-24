86f8ab805c0a0e00fa482abfbf19be561f03b0a0
James Thomas <jthomas.uk@gmail.com>
2019-06-24 15:00:06 +0100

    Fixing scancode issues. (#8)
    * Fixing issues discovered whilst developing new feed provider plugin.
    * Fixing all scancode issues.
    * Removing outdated JSHint tool from build process
    * Adding ASF license to Dockerfile

--- provider/app.js
+++ provider/app.js
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 'use strict';
 
 /**
@@ -33,7 +50,7 @@ for (let env_var of db_env_vars) {
   }
 }
 
-const dbUrl = process.env.DB_PROTOCOL + '://' + process.env.DB_USERNAME + ':' 
+const dbUrl = process.env.DB_PROTOCOL + '://' + process.env.DB_USERNAME + ':'
   + process.env.DB_PASSWORD + '@' + process.env.DB_HOST
 
 // This is the database that will store the managed triggers.
